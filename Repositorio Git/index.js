fc0a31a9f20288e4c5c8d06fb24aa055d9176721
MaxiWilliams <85195391+MaxiWilliams@users.noreply.github.com>
2023-07-11 20:11:10 -0300

    Add files via upload

---
+++ Repositorio Git/index.js
@@ -0,0 +1,35 @@
+//alert("hola, esta es una calculadora de prestamos bancarios")
+//alert("en el siguiente input ingrese la cantidad deseada a para su prestamo y cantidad de cuotas y el programa hara el trabajo")
+
+//const montoDeseado = prompt("Ingrese el monto deseado para su prestamo")
+//const cantidadDeCuotas = prompt("ahora la cantidad de cuotas")
+
+//let resultado = montoDeseado / cantidadDeCuotas
+
+//alert("por ahora, su prestamo sin interes queda en " + cantidadDeCuotas + " cuotas de $" + resultado)
+//alert("En el siguiente paso, ingrese su banco para calcular el interes del prestamo")
+
+//let banco = prompt("Ingrese su banco, las unicas posibilidades son: ICBC, Galicia, Santander, Nación. (Nota: escriba su banco exactamente como esta escrito en las opciones anteriores)")
+
+//switch(banco){
+//    case 'ICBC':
+//        let interesICBC = resultado * 1.05;
+//        alert("Con este banco tendra un interes del 5%, quedando asi en una deuda total de " + interesICBC);
+//    break;
+
+//    case 'Galicia':
+//        let interesGalicia = resultado * 1.07;
+//        alert("Con este banco tendra un interes del 7%, quedando asi en una deuda total de " + interesGalicia);
+//    break;
+
+//    case 'Santander':
+//        let interesSantander = resultado * 1.09;
+//        alert("Con este banco tendra un interes del 9%, quedando asi en una deuda total de " + interesSantander);
+//    break;
+
+//    case 'Nación':
+//        let interesNación = resultado * 1.12;
+//        alert("Con este banco tendra un interes del 12%, quedando asi en una deuda total de " + interesNación);
+//    break;
+//}
+
